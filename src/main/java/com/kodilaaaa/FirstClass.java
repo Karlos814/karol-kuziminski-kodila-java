8870e5fc3f829411e47ee616c6cb784359b50433
Karlos814 <karolkuziminski1@gmail.com>
2020-05-08 17:15:23 +0200

    reszta

---
+++ src/main/java/com/kodilaaaa/FirstClass.java
@@ -0,0 +1,4 @@
+package com.kodilaaaa;
+
+public class FirstClass {
+}
---
+++ src/test/java/com/kodilaaaa/KodilaaaaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilaaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodilaaaaCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
