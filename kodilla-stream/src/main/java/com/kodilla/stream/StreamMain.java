c5dbd2fb875fcd4cce303a5096809610586ee6b5
Karlos814 <karolkuziminski1@gmail.com>
2020-05-16 15:06:26 +0200

    Modul 7.3-main

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,18 +1,28 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
 
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 
-import javax.annotation.processing.Processor;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        String someTekst = "Tekst do przerobienia.";
 
-        poemBeautifier.beautify(someTekst, thisTekst -> "ABC"+thisTekst+"ABC");
-        poemBeautifier.beautify(someTekst, thisTekst -> thisTekst.toUpperCase());
-        poemBeautifier.beautify(someTekst, thisTekst -> thisTekst.replace('.', '?'));
-        poemBeautifier.beautify(someTekst, thisTekst -> thisTekst.substring(5).trim());
+
+        Forum usersList = new Forum();
+        Map<Integer, ForumUser> par = usersList.getUserList().stream()
+                .filter(m -> m.getSex()==('M'))
+                .filter(age -> ChronoUnit.YEARS.between(age.getDateOfBirth(), LocalDate.now()) >= 20)
+                .filter(post -> post.getQuantityOfPosts()>0)
+                .collect(Collectors.toMap(ForumUser::getUserID, user -> user));
+
+        System.out.println("# elements: " + par.size());
+        par.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
