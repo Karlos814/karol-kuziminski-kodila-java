cde2bfbf232895409602178bdf5684d0d48ce119
Karlos814 <karolkuziminski1@gmail.com>
2020-05-17 12:19:22 +0200

    Modul 7.5 i 7.6

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage(){
+        //Given
+        int [] tab = new int [] {1, 22, 8, 6, 3, 6, 7, 8, 7, 10, 13, 121, 0, 14};
+        double sum = 0.0;
+        for (int index:tab){
+            sum+=index;
+        }
+        double expected = sum/14.0;
+
+        //When
+        double actual = ArrayOperations.getAverage(tab);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0001);
+
+    }
+}
