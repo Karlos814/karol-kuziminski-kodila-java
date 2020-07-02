7f7e818cde10a6c65b70777f488f9a2358e7834d
Karlos814 <karolkuziminski1@gmail.com>
2020-07-02 20:24:40 +0200

    14.1

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.stream.beautifier;
-
-public class PoemBeautifier {
-
-    public void beautify(String tekst, PoemDecorator poemDecorator){
-        System.out.println("The tekst after decorate: "+poemDecorator.decorate(tekst));
-    }
-}
