44086702a8464af6e16fa321520b185b1f882c3e
Karlos814 <karolkuziminski1@gmail.com>
2020-07-23 17:12:36 +0200

    15.3

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.patterns.strategy.social;
+
+import com.kodilla.patterns.strategy.CorporateCustomer;
+import com.kodilla.patterns.strategy.Customer;
+import com.kodilla.patterns.strategy.IndividualCustomer;
+import com.kodilla.patterns.strategy.IndividualYoungCustomer;
+import com.kodilla.patterns.strategy.predictors.AggressivePredictor;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+    @Test
+    public void testDefaultSharingStrategies() {
+        //Given
+        User tomek = new Millenials("Tomasz Wons");
+        User krzys = new YGeneration("Krzysztof Grzyb");
+        User maro = new ZGeneration("Marek Iks");
+
+        //When
+        String tomekSocial = tomek.sharePost();
+        String krzysSocial = krzys.sharePost();
+        String maroSocial = maro.sharePost();
+
+        //Then
+        Assert.assertEquals("I use FaceBook", tomekSocial);
+        Assert.assertEquals("I use Twitter", krzysSocial);
+        Assert.assertEquals("I use Snapchat", maroSocial);
+    }
+
+    @Test
+    public void testIndividualSharingStrategy() {
+        //Given
+        User krzys = new YGeneration("Krzysztof Grzyb");
+
+        //When
+        krzys.setSocialPublisher(new SnapchatPublisher());
+        String krzysSocial = krzys.sharePost();
+
+        //Then
+        Assert.assertEquals("I use Snapchat", krzysSocial);
+    }
+}
