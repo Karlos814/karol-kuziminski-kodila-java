d5b45f0ad99e28cce96b728e2bb9ab90152076ec
Karlos814 <karolkuziminski1@gmail.com>
2020-10-09 19:36:48 +0200

    24.3

---
+++ kodilla-good-patterns2/src/main/java/kodillagoodpatterns2/facade/api/WatcherFacade.java
@@ -0,0 +1,18 @@
+package kodillagoodpatterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class WatcherFacade {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WatcherFacade.class);
+
+    @Before("execution(* kodillagoodpatterns2.facade.api.OrderFacade.processOrder(..))" + "&&target(object)")
+    public void logEvent(Object object) {
+        LOGGER.info("Method from class: " + object.getClass().getName());
+    }
+}
